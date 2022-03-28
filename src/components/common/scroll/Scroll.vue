f0d1295c522ce3d9b4234fab7f1353ce6cb9d4bf
jasonli <437147838@qq.com>
2022-03-28 16:14:50 +0800

    完成了betterscroll的封装

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,35 @@
+<template>
+    <div ref="scrollWrapper">
+        <div ref="scrollContent">
+            <slot>
+
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import bScroll from '@better-scroll/core';
+
+export default {
+    mounted(){
+        console.log(this.$refs.scrollWrapper);
+        new bScroll(this.$refs.scrollWrapper,{
+            probeType:3,
+        })
+    },
+    props:{
+        // width:{
+        //     type:Number,
+        //     default(){
+        //         return 100;
+        //     }
+        // }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
