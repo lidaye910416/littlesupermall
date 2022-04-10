27f7b3512f88a173567a23ef46a8679356ba4bf7
lidaye910416 <437147838@qq.com>
2022-04-10 23:01:53 +0800

    使用setup写了部分detail页面-220410

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,44 +1,108 @@
 <template>
     <div id="detail">
-        <h2>这里是详情页</h2>
+        <nav-bar class="nav-bar">
+            <template v-slot:middle-content>
+                <div class="middle-content">
+                    <div class="middle-content-titles" 
+                    v-for="(item, index) in detailInf.titles" 
+                    :key="index"
+                    :class="{active: index===detailInf.currIndex}"
+                    @click='detailInf.currIndex=index'>
+                        {{item}}
+                    </div>
+                </div>
+            </template>
+            <template v-slot:left-content>
+                <div class="left-content"  @click="goBack">
+                    <div class="left-content-back">
+                        <i class="iconfont icon-back">
+                            +
+                        </i>
+                    </div>
+                </div>
+            </template>        
+        </nav-bar>
+        <div >
+            <detail-swiper
+            :banners="getbannerImgs"  />
+        </div>
     </div>
   
 </template>
 
 <script>
-import navbar from 'components/common/navbar/NavBar.vue'
-import {useRoute} from 'vue-router';
-import {ref, reactive} from 'vue';
+import navbar from 'components/common/navbar/NavBar.vue';
+import detailswiper from './childcomp/DetailSwiper.vue';
+
+import {useRoute, useRouter}  from 'vue-router';
+import {reactive, onBeforeMount, computed} from 'vue';
 
 import {getDetailInfo} from 'network/home';
 export default {
+    components: {
+        'nav-bar': navbar,
+        'detail-swiper': detailswiper,
+        
+    },
     setup(){
-        //以下代码用于测试
+        //学习使用setup
         const route = useRoute();
+        const router = useRouter();
         // let tid =ref(route.params.id);
-
         const detailInf = reactive({
-            banners:[],
             tid:route.params.id,
+            detailResult:{},
+            titles:['商品','参数','评价','详情'],
+            currIndex:0
         });
-        
-        //获取详情页的网络请求
-        (function getDetailImg(){
-            console.log(detailInf.tid);
+        onBeforeMount(()=>{
             getDetailInfo(detailInf.tid).then(res=>{
-                console.log(res);
+                detailInf.detailResult = res;
             })
-        })();
+        });
 
+        let getbannerImgs = computed(()=>{
+            //以下代码会报错，最简单的处理是用try 包裹一下
+            return detailInf.detailResult.result.itemInfo.topImages;
+        });        
 
-        
+        //获取详情页的网络请求
+        // function getDetailImg(){
+        //     getDetailInfo(detailInf.tid).then(res=>{
+        //         detailInf.detailResult = res;
+
+        //     })
+        // };
+
+        function goBack(){
+            router.go(-1);
+        };
         return {
             detailInf,
+            goBack,
+            getbannerImgs
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
 
+.middle-content{
+    display: flex;
+    justify-content: space-between;
+}
+
+.middle-content-titles {
+    flex: 1;
+}
+
+.active{
+    color: #ff6700;
+}
+
+.left-content{
+    width: 100%;
+    height: 100%;
+}
 </style>
---
+++ src/views/detail/childcomp/DetailSwiper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="detail-swiper">
+        <swiper>
+            <template v-slot:default>
+                <swiper-item v-for="(item, index) in banners" :key="index">
+                    <template v-slot:img>
+                        <!-- <a :href="item.link"> -->
+                            <img class="swiper-img" :src="item" alt="" @load="swiperLoaded">
+                        <!-- </a> -->
+                    </template>
+                </swiper-item>
+            </template>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import swiper from 'components/common/swiper/Swiper.vue'
+import swiperitem from 'components/common/swiper/SwiperItem.vue'
+
+export default {
+    components:{
+        swiper,
+        'swiper-item': swiperitem
+    },
+    props:{
+        banners:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    },
+    setup(){
+        
+    }
+    
+}
+</script>
+
+<style>
+    .swiper-img{
+        height: 300px;
+        object-fit: cover;
+    }
+</style>
